bb54179487035b464076ccc7b0f4590eeb0eec51
Misty Bui <mishtybui@gmail.com>
2020-01-15 13:25:45 +0200

    fetch data with ajax, promise all

--- components/ListItem.js
+++ components/ListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import PropTypes from 'prop-types';
 
+const mediaUrl ='http://media.mw.metropolia.fi/wbma/uploads/';
 
 const ListItem = (props) => {
   console.log(props.singleMedia);
@@ -10,7 +11,7 @@ const ListItem = (props) => {
     <TouchableOpacity style={styles.touch}>
       <Image
         style={{width: 100, height: 100}}
-        source={{uri: singleMedia.thumbnails.w160}}
+        source={{uri: mediaUrl + props.singleMedia.filename}}
       />
       <View style={styles.text}>
         <Text style={{fontWeight: 'bold'}}>{singleMedia.title}</Text>
